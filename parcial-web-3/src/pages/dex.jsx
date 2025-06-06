8fc75c2c7012cde27c57987d141a2640f533f386
daniel <145810043+dan0407@users.noreply.github.com>
2025-06-05 19:33:55 -0500

    fin fin

--- parcial-web-3/src/pages/dex.jsx
+++ parcial-web-3/src/pages/dex.jsx
@@ -6,19 +6,23 @@ import { useEffect, useState } from 'react';
 const Dex =()=>{
 
     const [datadigi,setdatadigi]=useState([])
+console.log(datadigi);
 
 	useEffect(() => {
 		fetch('https://digi-api.com/api/v1/digimon?pageSize=25')
 			.then((response) => response.json())
-			.then((data) => setdatadigi(data))
+			.then((data) => setdatadigi(data.content))
 			
 	}, []);
 
 
     return (
 		<div>
-			<h2>digimon</h2>
-			<p>digimon{datadigi.name}</p>
+			{
+                datadigi.map((d) => {
+                    return <h1>{d.name}</h1>
+                })
+            }
 		</div>
 	);
 
